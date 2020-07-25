044917705c12c2e6af4ed4522790ac8bdf27077f
a <a@a>
2020-07-25 09:53:37 -0700

    dns sinkhole

---
+++ README.md
@@ -0,0 +1,15 @@
+# DNS sinkhole
+
+Install unbound dns
+
+Upate the root.hints, this can be in a cron every 6 months (`0 * * */6 *`)
+
+`wget -c http://www.internic.net/domain/named.root -O /var/lib/unbound/root.hints`
+
+Edit the unbound.conf and adjust settings according to your needs
+
+You can add or remove more lists in the denylist.sh run in a cron every week (`0 2 * * 1`)
+
+Install some ad blocker in your browser for better results
+
+I have this running in a old raspberry pi with [dietpi](https://dietpi.com/) and works fine
---
+++ denylist.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+URLS=(
+https://adaway.org/hosts.txt                                                                                                                                    
+https://urlhaus.abuse.ch/downloads/hostfile/                                                                                                                    
+https://mirror1.malwaredomains.com/files/justdomains                                                                                                            
+https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt                                                                                                      
+https://codeberg.org/spootle/blocklist/raw/branch/master/blocklist.txt                                                                                          
+https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
+https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt
+https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt
+https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt
+https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt
+https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt
+https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+https://phishing.army/download/phishing_army_blocklist_extended.txt
+https://pgl.yoyo.org/adservers/serverlist.php\?hostformat=hosts\&showintro=0\&mimetype=plaintext
+)
+
+( for url in ${URLS[@]}; do curl -L -s $url; done ) \
+ | sed '/^s*$/d;/^\s*\#/d;/"/d;s/$\s//;s///;s/[[:blank:]]*$//;s/\(\.*\)\(#.*\)/\1/;s/\.$//' \
+ | awk '{print $NF}' | tr '[:upper:]' '[:lower:]' | sort | uniq \
+ | awk '{print "local-zone: \""$1"\" always_nxdomain"}' > /tmp/denylist.conf
+
+# remove domain from denylist
+sed -i '/algolia.com/d' /tmp/denylist.conf
+sleep 1
+sudo mv -v /etc/unbound/denylist.conf ~/denylist.conf.old
+sudo mv -v /tmp/denylist.conf /etc/unbound/denylist.conf
+sync
+sleep 1
+sudo systemctl stop unbound
+sleep 1
+sudo systemctl start unbound
